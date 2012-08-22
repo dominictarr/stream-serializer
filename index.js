a485858831ddb14930fde3eaf1cc475c9c96724e
Dominic Tarr <dominic.tarr@gmail.com>
2012-08-23 01:09:41 +0200

    more automatic interface

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 var es = require('event-stream')
 
+exports = module.exports = function (wrapper) {
+
+  if('function' == typeof wrapper)
+    return wrapper
+  
+  return exports[wrapper] || exports.json
+}
+
 exports.json = function (stream) {
   return es.pipeline(es.split(), es.parse(), stream, es.stringify())
 }
