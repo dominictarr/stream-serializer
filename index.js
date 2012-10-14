a5c777edd303e8867bd30df368b2f9de5855d5bb
Dominic Tarr <dominic.tarr@gmail.com>
2012-10-15 00:05:45 +0200

    patch given stream to make it json-lines

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 
-var es = require('event-stream')
-
 exports = module.exports = function (wrapper) {
 
   if('function' == typeof wrapper)
@@ -10,7 +8,54 @@ exports = module.exports = function (wrapper) {
 }
 
 exports.json = function (stream) {
-  return es.pipeline(es.split(), es.parse(), stream, es.stringify())
+
+  var write = stream.write
+  var soFar = ''
+
+  function parse (line) {
+    try {
+      var js = JSON.parse(line)
+      //ignore lines of whitespace...
+      if(js !== undefined)
+        write.call(stream, js)
+    } catch (err) { 
+      console.error('invalid JSON', data)
+    }
+  }
+
+  function onData (data) {
+    var lines = (soFar + data).split('\n')
+    soFar = lines.pop()
+    while(lines.length) {
+      parse(lines.shift())
+    }
+  }
+
+  stream.write = onData
+  
+  var end = stream.end
+
+  stream.end = function (data) {
+    if(data)
+      stream.write(data)
+    //if there is any left over...
+    if(soFar) {
+      parse(soFar)
+    }
+    return end.call(stream)
+  }
+
+  stream.emit = function (event, data) {
+
+    if(event == 'data') {
+      data = JSON.stringify(data) + '\n'
+    }
+    //since all stream events only use one argument, this is okay...
+    EventEmitter.prototype.emit.call(stream, event, data)
+  }
+
+  return stream
+//  return es.pipeline(es.split(), es.parse(), stream, es.stringify())
 }
 
 exports.raw = function (stream) {
