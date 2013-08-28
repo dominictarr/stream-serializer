88b45ffa1e1fb9a42287d8510e45b82b867ac158
Dominic Tarr <dominic.tarr@gmail.com>
2013-08-28 12:24:56 +0100

    more tests

--- test/json.js
+++ test/json.js
@@ -1,29 +1,31 @@
 
 var through   = require('through')
 var serialize = require('..')
-var assert    = require('assert')
 
-var expected = [1, 2, 3], actual = []
-var ended = false
+require('tape')('json messages', function (t) {
 
-var ss = serialize()(through(function (data) {
-  var n = expect = expected.shift()
-  assert.equal(data, n)
-  console.log(data)
-  this.emit('data', data)
-}, function () {
-  ended = true
-}))
+  var expected = [1, 2, 3], actual = []
+  var ended = false
+
+  var ss = serialize()(through(function (data) {
+    var n = expect = expected.shift()
+    t.equal(data, n)
+    console.log(data)
+    this.emit('data', data)
+  }, function () {
+    ended = true
+  }))
 
 
-ss.on('data', function (d) {
-  console.log('>>', d, '<<')
-  actual.push(d)
+  ss.on('data', function (d) {
+    console.log('>>', d, '<<')
+    actual.push(d)
+  })
+  ss.write('1\n2\n3\n')
+  ss.end()
+
+  t.equal(expected.length, 0)
+  t.deepEqual(actual, ['1\n', '2\n', '3\n'])
+  t.equal(ended, true)
+  t.end()
 })
-ss.write('1\n2\n3\n')
-ss.end()
-
-assert.equal(expected.length, 0)
-assert.deepEqual(actual, ['1\n', '2\n', '3\n'])
-assert.equal(ended, true)
-
